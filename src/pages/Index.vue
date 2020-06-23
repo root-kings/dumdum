c3723e2107261e9e1494320eeef551d1bf6d2e0c
krushndayshmookh <krushndayshmookh@gmail.com>
2020-06-23 14:10:59 +0530

    :sparkles: fetch compliments from https://complimentr.com/

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-  
+    {{ compliment }}
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex",
+  data() {
+    return {
+      compliment: ""
+    };
+  },
+  methods: {
+    fetchCompliment() {
+      this.$axios.get("https://complimentr.com/api").then(response => {
+        this.compliment = response.data.compliment;
+      });
+    }
+  },
+  mounted(){
+    this.fetchCompliment()
+  }
+};
 </script>
